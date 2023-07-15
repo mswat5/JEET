fb827f2a5023778226b98be986745445facf53f8
mswat5 <mswat14361@gmail.com>
2023-07-16 01:26:13 +0530

    landing page setup

---
+++ jeet/src/components/Header.js
@@ -0,0 +1,13 @@
+// components/Header.js
+import React from 'react';
+// import {Dark} from './components/Dark';
+
+const Header = () => {
+  return (
+    <header className="bg-blue-500 text-white p-4 md:p-8">
+             {/* <Dark />  */}
+    </header>
+  );
+};
+
+export default Header;
