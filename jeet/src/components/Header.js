442c6c633d6e449b9e88dcb686b9f5f8f27028d4
mswat5 <mswat14361@gmail.com>
2023-07-16 02:05:48 +0530

    header color

--- jeet/src/components/Header.js
+++ jeet/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-blue-500 text-white p-4">
+    <header className="bg-orange-400 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-xl font-semibold"><img src="" alt="JEET LO"/></div>
         <nav>
