8ee129930cdd979451b1243c8e1b48936b505154
mswat5 <mswat14361@gmail.com>
2023-07-16 01:42:56 +0530

    swatantra rights

--- jeet/src/components/Footer.js
+++ jeet/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Swatantra Mishra. All rights reserved.</p>
       </div>
     </footer>
   );
